c9956acb355ded756d74749db667c333f113aafd
Gruck <gael.motte@gmail.com>
2015-11-16 08:30:42 +0100

    add test for draw solutions elimination

--- test2.rb
+++ test2.rb
@@ -16,12 +16,15 @@ class TestCSA < Test::Unit::TestCase
 1 2 3600 7200
 2 3 4000 5000
 2 3 8000 9000
-1 3 4999 10000
 1 3 5000 10000
-1 3 5001 10000
-1 3 5002 10000
-1 3 5003 10000
-1 3 5004 10000
+5 8 20000 25000
+5 8 21000 25000
+5 6 22000 23000
+5 7 23000 23500
+7 8 24000 25000
+6 8 24000 25000
+5 7 33100 33500
+7 8 34000 35000
 
 EOF
 
@@ -91,7 +94,6 @@ EOF
     assert_equal 1, response[2][:departure_station]
     assert_equal 3, response[2][:arrival_station]
     assert_equal 10000, response[2][:arrival_timestamp]
-
   end
 
   def test_later_departure
@@ -127,5 +129,36 @@ EOF
     assert_equal 2, response1.size
     assert_equal 2, response2.size
   end
+
+  def test_draw_elimination
+    puts "test_draw_elimination"
+    #both fastest and least connection may encounter draw solutions on this criteria, we need to filter out least worthy solutions.
+    ###Fastest : 
+    #1. find the solutions that arrive at the earliest possible date.
+    #2. filter those that depart the latest.
+    #3. choose any of these at random, no sepcified dehavior // too little chances we might end up with more than 1.
+    ###Least COnnections :
+    #0. find solutions with the least number of connections
+    #1. filter the solutions that arrive at the earliest possible date.
+    #2. filter those that depart the latest.
+    #3. choose any of these at random, no sepcified dehavior // too little chances we might end up with more than 1.
+    @io.puts "5 8 19000"
+    response = read_answer @io
+    assert_equal 3, response.size
+    assert_equal 1, response[0][:solution_type]
+    assert_equal 5, response[0][:departure_station]
+    assert_equal 7, response[0][:arrival_station]
+    assert_equal 23500, response[0][:arrival_timestamp]
+    assert_equal 1, response[1][:solution_type]
+    assert_equal 7, response[1][:departure_station]
+    assert_equal 8, response[1][:arrival_station]
+    assert_equal 25000, response[1][:arrival_timestamp]
+    assert_equal 2, response[2][:solution_type]
+    assert_equal 5, response[2][:departure_station]
+    assert_equal 8, response[2][:arrival_station]
+    assert_equal 21000, response[2][:departure_timestamp]
+    assert_equal 25000, response[2][:arrival_timestamp]
+  end
+
 
 end
