63d882e9288cda88b7735f4ddf2220cbfa21489a
Gruck <gael.motte@gmail.com>
2015-11-18 00:24:57 +0100

    now passes test2.rb

--- csa2.rb
+++ csa2.rb
@@ -5,6 +5,7 @@ require "yaml"
 
 MAX_STATIONS = 20
 INF = 1.0/0
+TRANSFER_TIME_THRESHOLD = 900
 
 class Connection
   attr_reader :departure_station, :arrival_station,
@@ -38,13 +39,14 @@ end
 class Route_Step
   include Comparable
   attr_reader :trip_departure_timestamp, :arrival_timestamp,
-              :connection_count, :index_in_timetable, :previous_step, :comparator_earliestarrival
+              :connection_count, :index_in_timetable, :previous_step, :min_transfer_time
 
-  def initialize(trip_departure_timestamp, arrival_timestamp, connection_count, index_in_timetable, previous_step)
+  def initialize(trip_departure_timestamp, arrival_timestamp, connection_count, index_in_timetable, previous_step,min_transfer_time)
     @trip_departure_timestamp = trip_departure_timestamp
     @arrival_timestamp = arrival_timestamp
     @connection_count = connection_count
     @index_in_timetable = index_in_timetable
+    @min_transfer_time = min_transfer_time
     @previous_step = previous_step
   end
 end
@@ -68,7 +70,8 @@ class CSA
                                                               c.arrival_timestamp,
                                                               previous.connection_count+1,
                                                               i,
-                                                              previous
+                                                              previous,
+                                                              previous.connection_count >= 0  ? [previous.min_transfer_time, c.departure_timestamp - previous.arrival_timestamp].min : previous.min_transfer_time
                                                             )
         end
       end
@@ -76,29 +79,41 @@ class CSA
 
   end
 
-  def print_result(arrival_station)
+  def has_result_or_report(arrival_station)
     if possible_routes[arrival_station].length == 0
       puts "NO_SOLUTION"
+      return false
     else
-      route = []
-      # We have to rebuild the route from the arrival station
-      possible_routes[arrival_station].sort_by {|e| e.arrival_timestamp}
-      current_route_step = possible_routes[arrival_station][0]
-
-      while current_route_step.connection_count != -1
-        route << timetable.connections[current_route_step.index_in_timetable]
-        current_route_step = current_route_step.previous_step
-      end
-
-
-      # And now print it out in the right direction
-      route.reverse.each do |c|
-        STDERR.puts "#{c.departure_station} #{c.arrival_station} #{c.departure_timestamp} #{c.arrival_timestamp}"
-        puts "#{c.departure_station} #{c.arrival_station} #{c.departure_timestamp} #{c.arrival_timestamp}"
-      end
+      return true
     end
-    puts ""
-    STDOUT.flush
+  end
+
+  def compute_route(arrival_station, order_criteria, filter_criteria)
+    route = []
+    # We have to rebuild the route from the arrival station
+    possibilities = possible_routes[arrival_station].select &filter_criteria
+    #STDERR.puts YAML::dump(possibilities)
+    possibilities = possibilities.sort_by &order_criteria
+    #STDERR.puts YAML::dump(possibilities)
+    current_route_step = possibilities[0]
+
+    has_short_transfers = current_route_step.min_transfer_time < TRANSFER_TIME_THRESHOLD
+
+    while current_route_step.connection_count != -1
+      route << timetable.connections[current_route_step.index_in_timetable]
+      current_route_step = current_route_step.previous_step
+    end
+
+    return route.reverse, has_short_transfers  #TODO : add second return value has_short_transfers using destructuration 
+  end
+
+  def print_result(route, solution_type_prefix)   
+    
+    route.each do |c|
+      STDERR.puts "#{solution_type_prefix} #{c.departure_station} #{c.arrival_station} #{c.departure_timestamp} #{c.arrival_timestamp}"
+      puts "#{solution_type_prefix} #{c.departure_station} #{c.arrival_station} #{c.departure_timestamp} #{c.arrival_timestamp}"
+    end
+    
   end
 
   def compute(departure_station, arrival_station, departure_time)
@@ -115,7 +130,8 @@ class CSA
                                                             departure_time,
                                                             -1,
                                                             nil,
-                                                            nil
+                                                            nil,
+                                                            INF
                                                             )
 
         
@@ -123,7 +139,24 @@ class CSA
       main_loop()
     end
 
-    print_result(arrival_station)
+    if(has_result_or_report(arrival_station))
+      route, has_short_transfers = compute_route(arrival_station, lambda  {|e| [e.arrival_timestamp, -e.trip_departure_timestamp]}, lambda {|e| true})
+      print_result(route, "EARLIEST_ARRIVAL") 
+      if(has_short_transfers)
+        route, has_short_transfers = compute_route(arrival_station, lambda  {|e| [e.arrival_timestamp, -e.trip_departure_timestamp]}, lambda {|e| e.min_transfer_time > TRANSFER_TIME_THRESHOLD})
+        print_result(route, "EARLIEST_ARRIVAL_WITH_EASY_TRANSFERS")  
+      end 
+      route, has_short_transfers = compute_route(arrival_station, lambda  {|e| [e.connection_count, e.arrival_timestamp, -e.trip_departure_timestamp]}, lambda {|e| true})
+      print_result(route, "LEAST_CONNECTIONS")
+      if(has_short_transfers)
+        route, has_short_transfers = compute_route(arrival_station, lambda  {|e| [e.connection_count, e.arrival_timestamp, -e.trip_departure_timestamp]}, lambda {|e| e.min_transfer_time > TRANSFER_TIME_THRESHOLD})
+        print_result(route, "LEAST_CONNECTIONS_WITH_EASY_TRANSFERS")  
+      end 
+    end
+
+    puts ""
+    STDOUT.flush
+
   end
 end
 
